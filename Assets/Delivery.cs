1300d1e66a8418e5a357b4b07c757c18bac5dbdb
Emily Krohn <emilykrohn2@gmail.com>
2024-06-03 10:40:49 -0700

    Use GetComponent the change color
    When player picks up a package, they turn green and when they deliver the package, they turn back to the original color.

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
     [SerializeField] float destroyPackageDelay = 0.5f;
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -18,12 +27,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up.");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyPackageDelay);
         }
         else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Delivered package.");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
